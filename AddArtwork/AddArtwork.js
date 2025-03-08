c245f63d0330713755a6c9b4ebe113a01ed557e0
05rana <rana.bridi@gmail.com>
2025-03-08 03:22:40 +0300

    AddArtwork final edit

--- AddArtwork/AddArtwork.js
+++ AddArtwork/AddArtwork.js
@@ -80,3 +80,22 @@ document.addEventListener("DOMContentLoaded", function () {
         field.addEventListener("focus", ensureDollarSign); // Adds "$" if empty when clicked
     });
 });
+	// JavaScript to handle the clicks for the elements
+	document.addEventListener("DOMContentLoaded", function() {
+
+        // Redirect to index.html when clicking on the logo
+        document.querySelector(".logo").addEventListener("click", function() {
+            window.location.href = "../Home/index.html";
+        });
+        
+        // Redirect to Wishlist.html when clicking on the wishlist icon
+        document.querySelector("#wishlist-header").addEventListener("click", function() {
+            window.location.href = "../Wishlist/Wishlist.html";
+        });
+        
+        // Redirect to Profile.html when clicking on the profile icon
+        document.querySelector("#profile-header").addEventListener("click", function() {
+            window.location.href = "../Profile/Profile.html";
+        });
+        
+        });
